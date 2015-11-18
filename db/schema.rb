5017ebe4ca090c40b1bbf157c1e3918daaa07e0d
John Park <Mr_MegaGigabytes@Johns-MacBook-Air.local>
2015-11-18 18:18:33 -0500

    Adding paperclip to include images

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118183457) do
+ActiveRecord::Schema.define(version: 20151118215650) do
 
   create_table "requests", force: :cascade do |t|
     t.string   "pet_name"
     t.text     "description"
     t.text     "care_instructions"
     t.date     "range"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
