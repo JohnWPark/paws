67595f744bf87eeabbefd174cecf07615611d8ef
John Park <jwpark417@gmail.com>
2016-02-14 15:35:47 -0500

    Adding figaro gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.5'
 # added paperclip gem
 gem "paperclip", "~> 4.3"
+gem "paperclip-dropbox", ">= 1.1.7"
+gem "figaro"
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
 
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,16 @@
 class Request < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
+  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" },
+  default_url: "default.jpg",
+  :storage => :dropbox,
+   :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  #  :dropbox_options => {...}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 
   # Validates form fields are present
   validates :pet_name, :description, :care_instructions, presence: true
   validates_attachment_presence :image
 
   belongs_to :user
+
 end
