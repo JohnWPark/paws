93aa1e88770aaf32b271150e754f966d37082720
John Park <jwpark417@gmail.com>
2016-02-15 14:41:54 -0500

    Finishing setup for Dropbox

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,14 @@
 class Request < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" },
-  default_url: "default.jpg",
-  :storage => :dropbox,
-   :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" },
+    default_url: "default.jpg"
+  else
+    has_attached_file :image, styles: => { medium: "200x", thumb: "100x100>" },
+    default_url: "default.jpg",
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
+  end
   #  :dropbox_options => {...}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
